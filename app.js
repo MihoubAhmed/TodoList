4657856216da4ce2e890b1a35e1fa39844f90284
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-05-23 11:13:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express= require("express");
+
+var app = express();
+
+app.get('/todo',function(req, res){
+	
+	
+})
+.post('/todo/ajouter',function(req, res){
+	
+	
+})
+.get('/todo/supprimer/:id',function(req, res){
+	
+	
+})
+app.listen(8080);
