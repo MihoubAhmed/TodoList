7ffdfc84e130fd139d28230618db1b1fc40662c7
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-05-23 12:02:22 +0100

    implement routes

--- app.js
+++ app.js
@@ -5,19 +5,38 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var app = express();
 
 
-/* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
 
-app.get('/todo',function(req, res){
-	
-	
+/* On affiche la todolist et le formulaire */
+.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
 })
-.post('/todo/ajouter',function(req, res){
-	
-	
+
+/* On ajoute un élément à la todolist */
+.post('/todo/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
 })
-.get('/todo/supprimer/:id',function(req, res){
-	
-	
+
+/* Supprime un élément de la todolist */
+.get('/todo/supprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* On redirige vers la todolist si la page demandée n'est pas trouvée */
+.use(function(req, res, next){
+    res.redirect('/todo');
 })
 app.listen(8080);
